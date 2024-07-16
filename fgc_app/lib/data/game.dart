64beaf8ce97985a5ca87e7995d5814d8a2c355a4
20970594 <65563260+20970594@users.noreply.github.com>
2024-07-16 10:15:21 -0400

    revertir

--- fgc_app/lib/data/game.dart
+++ fgc_app/lib/data/game.dart
@@ -46,21 +46,4 @@ const Game({
   String toString() {
     return 'Game{id: $id, name: $name, year: $year, genre: $genre, description: $description, urlAccess: $urlAccess, punctuation: $punctuation}';
   }
-
-  //Game(this.name, this.urlLogo, this.urlAccess, this.description, this.genre);
-
-  /*String getName(){return name;}
-  void setName(String value){name=value;}
-
-  String getUrlLogo(){return urlLogo;}
-  void setUrlLogo(String value){urlLogo=value;}
-
-  String getUrlAccess(){return urlAccess;}
-  void setUrlAccess(String value){urlAccess=value;}
-
-  String getDescription(){return description;}
-  void setDescription(String value){description=value;}
-
-  String getGenre(){return genre;}
-  void setGenre(String value){genre=value;}*/
 }
